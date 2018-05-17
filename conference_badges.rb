c04eb211447e766778cf139c446d45c11c52e76b
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-17 23:49:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hellow, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(array)
+  
+end
