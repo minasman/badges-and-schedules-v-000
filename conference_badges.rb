411e2f76c68a30e82a96f2d1b906ecb53e7d7c7d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-17 23:59:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(array)
   rooms = []
   array.each_with_index do |name, i|
-    rooms.push("Hello, #{name}! You'll be assigned to room #{i + 1}")
+    rooms.push("Hello, #{name}! You'll be assigned to room #{i + 1}!")
   end
   return rooms
 end
