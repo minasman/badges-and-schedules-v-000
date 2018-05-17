ea6000bdacca235c36ac33b1ed06ab95578370c0
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-17 23:54:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,22 @@
 # Write your code here.
 def badge_maker(name)
-  return "Hellow, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
+  badges = []
   array.each do |name|
-    badge_maker(name)
+    badges.push(badge_maker(name))
   end
+  return badges
 end
 
 def assign_rooms(array)
+  rooms = []
+  array.each_with_index do |name|
+    rooms.push("Hello, #{name}! You'll be assigned to room ")
+end
+
+def printer(badges, rooms)
   
 end
